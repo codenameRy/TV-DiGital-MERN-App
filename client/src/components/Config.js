917af116e9db7d633ba326a6605152d89ff8feca
Ryan Lee <ryanm.lee11@gmail.com>
2020-05-24 17:42:38 -0400

    Update

---
+++ client/src/components/Config.js
@@ -0,0 +1,4 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_KEY = 'a0265ab770ca0c045998969cf812d64f';
---
+++ server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://mongorl:wotest186x@cluster1-ticd3.gcp.mongodb.net/test?retryWrites=true&w=majority'
+}
