558b28760a26776cdbd2a474ecd136d863b9859a
Ryan Lee <ryanm.lee11@gmail.com>
2020-05-26 18:09:23 -0400

    update

--- server/index.js
+++ server/index.js
@@ -1,27 +1,40 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
+const cors = require("cors");
 
 const mongoose = require("mongoose");
-const connect = mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB Connected to Atlas...'))
-  .catch(err => console.log(err));
+const connect = mongoose
+  .connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected to Atlas..."))
+  .catch((err) => console.log(err));
+
+app.use(
+  cors({
+    credentials: true,
+    origin: [
+      "http://localhost:3000",
+      "https://quiet-forest-60957.herokuapp.com/",
+    ], //Swap this with the client url
+  })
+);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use('/api/users', require('./routes/users'));
+app.use("/api/users", require("./routes/users"));
 
 //use this to show the image you have in node js server to client (react js)
 //https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
-
   // Set static folder
   app.use(express.static("client/build"));
 
@@ -31,8 +44,8 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Server Running at ${port}`)
+  console.log(`Server Running at ${port}`);
 });
