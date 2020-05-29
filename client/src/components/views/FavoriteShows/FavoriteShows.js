211287620fae852ed524e5c356c132ab3bff4c02
Ryan Lee <ryanm.lee11@gmail.com>
2020-05-29 09:56:41 -0400

    styling update

--- client/src/components/views/FavoriteShows/FavoriteShows.js
+++ client/src/components/views/FavoriteShows/FavoriteShows.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-import { Button } from 'antd';
+// import { Button } from 'antd';
+import {Col} from 'antd';
+
 // import { useSelector } from 'react-redux';
 
 function FavoriteShows(props) {
@@ -83,7 +85,29 @@ function FavoriteShows(props) {
 
     return (
         <>
-            <Button onClick={onClickFavorite}> {!Favorited ? "Add to Favorite" : "Not Favorite"} </Button>
+            {/* <Button onClick={onClickFavorite}> {!Favorited ? "Add to Favorite" : "Not Favorite"} </Button> */}
+            {/* <div>
+            <img src="https://image.tmdb.org/t/p/w500/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg" alt="Girl in a jacket" width="258" height="320"></img>
+            <img src="https://image.tmdb.org/t/p/w500/f496cm9enuEsZkSPzCwnTESEK5s.jpg" alt="Girl in a jacket" width="258" height="320"></img>
+            <img src="https://image.tmdb.org/t/p/w500/u3bZgnGQ9T01sWNhyveQz0wH0Hl.jpg" alt="Girl in a jacket" width="258" height="320"></img>
+            <img src="https://image.tmdb.org/t/p/w500/y55oBgf6bVMI7sFNXwJDrSIxPQt.jpg" alt="Girl in a jacket" width="258" height="320"></img>
+            <img src="https://image.tmdb.org/t/p/w500/y55oBgf6bVMI7sFNXwJDrSIxPQt.jpg" alt="Girl in a jacket" width="258" height="320"></img>
+            <img src="https://image.tmdb.org/t/p/w500/x2LSRK2Cm7MZhjluni1msVJ3wDF.jpg" alt="Girl in a jacket" width="258" height="320"></img>
+            <img src="https://image.tmdb.org/t/p/w500/9BvRze9keEyTzB6ewmDvjzFGYuG.jpg" alt="Girl in a jacket" width="258" height="320"></img>
+            </div> */}
+            
+            <Col lg={6} md={8} sm={16} xs={24}>
+                 <div style={{ position: 'relative' }}>
+                    
+                        <img style={{ width: '258px', height: '320px' }} alt={'temp'} src="https://image.tmdb.org/t/p/w500/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg"/>
+                        <img style={{ width: '258px', height: '320px' }} alt={'temp'} src="https://image.tmdb.org/t/p/w500/f496cm9enuEsZkSPzCwnTESEK5s.jpg"/>
+                        <img style={{ width: '258px', height: '320px' }} alt={'temp'} src="https://image.tmdb.org/t/p/w500/u3bZgnGQ9T01sWNhyveQz0wH0Hl.jpg"/>
+                        <img style={{ width: '258px', height: '320px' }} alt={'temp'} src="https://image.tmdb.org/t/p/w500/y55oBgf6bVMI7sFNXwJDrSIxPQt.jpg"/>
+                        <img style={{ width: '258px', height: '320px' }} alt={'temp'} src="https://image.tmdb.org/t/p/w500/9BvRze9keEyTzB6ewmDvjzFGYuG.jpg"/>
+                    
+                </div>
+            </Col>
+
         </>
     )
 }
